5e16ef66a03efea0d36964b3c5d5aac3e2915726
Jack Maguire <jackmmaguire@proton.me>
2023-12-15 09:34:45 +0000

    d3p1

--- crates/day3/src/main.rs
+++ crates/day3/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashSet, ops::RangeInclusive};
 
 fn main() {
     let input = include_str!("input.txt");
@@ -9,32 +9,35 @@ fn main() {
     println!("{n}");
 }
 
-fn part1 (parts: HashMap<(usize, usize), PartNumber>) -> u32 {
-    parts.values().map(|x| x.number).sum()
+fn part1 (parts: Vec<PartNumber>) -> u32 {
+    parts.into_iter().map(|x| x.number).sum()
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Clone, Debug)]
 pub struct PartNumber {
+    pub location: (RangeInclusive<usize>, usize),
     pub number: u32,
     pub symbol_discovered_by: char,
 }
 
+///NB: assumes that (x, y) is inbounds, and (dx, dy) is in [-1, 1]
 fn get_coord_in_bounds ((x, y): (usize, usize), (dx, dy): (i32, i32), width: usize, height: usize) -> Option<(usize, usize)> {
     let mut nx = None;
     let mut ny = None;
 
-    if (dx < 0 && x > 1) || (dx > 0 && x < (width - 2)){
+    if (x == 0 && dx >= 0) || (x == width - 1 && dx <= 0) || (x > 0 && x < width - 1) {
         nx = Some((x as i32 + dx) as usize);
-    }
-    if (dy < 0 && y > 1) || (dy > 0 && y < (height - 2)) {
+    } 
+    if (y == 0 && dy >= 0) || (y == height - 1 && dy <= 0) || (y > 0 && y < height - 1) {
         ny = Some((y as i32 + dy) as usize);
     }
-
+    
     nx.zip(ny)
 }
 
-fn get_part_numbers (input: &str) -> HashMap<(usize, usize), PartNumber> {
-    let mut list = HashMap::new();
+fn get_part_numbers (input: &str) -> Vec<PartNumber> {
+    let mut list = vec![];
+    let mut already_found = HashSet::new();
 
     let lines = input.lines().collect::<Vec<_>>();
 
@@ -46,7 +49,6 @@ fn get_part_numbers (input: &str) -> HashMap<(usize, usize), PartNumber> {
     for (y, chars) in lines_as_char_arrays.clone().into_iter().enumerate() {
         for (x, discovering_char) in chars.into_iter().enumerate() {
             if discovering_char != '.' && !discovering_char.is_ascii_digit() {
-                //we're at a symbol
 
                 for delta in [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)] {
                     if let Some(location) = get_coord_in_bounds((x, y), delta, width, height) {
@@ -79,13 +81,13 @@ fn get_part_numbers (input: &str) -> HashMap<(usize, usize), PartNumber> {
                                 }
                             }
 
-                            let st = (&lines_as_char_arrays[y][left_most..=right_most]).iter().collect::<String>();
-                            println!("{st:?}");
-                            let number = st.parse().unwrap();
-                            println!("{number}");
-                            
+                            let location = (left_most..=right_most, y);
+                            let number = (&lines_as_char_arrays[y][left_most..=right_most]).iter().collect::<String>().parse().unwrap();                           
 
-                            list.insert(location, PartNumber { number, symbol_discovered_by: discovering_char });
+                            if !already_found.contains(&location) {
+                                already_found.insert(location.clone());
+                                list.push(PartNumber { location, number, symbol_discovered_by: discovering_char });
+                            }
                         }
                     }
                 }
