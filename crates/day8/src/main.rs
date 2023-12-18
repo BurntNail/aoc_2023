d56090411795c2c0d26d9c4646c04ffd9a5fb498
Jack Maguire <jackmmaguire@proton.me>
2023-12-18 21:16:37 +0000

    d8p2 inf

--- crates/day8/src/main.rs
+++ crates/day8/src/main.rs
@@ -8,41 +8,59 @@ fn main() {
 
     let directions: Vec<bool> = lines.next().unwrap().chars().map(|c| c == 'L').collect();
     let _ = lines.next();
-    let (start, target, graph) = parse_graph(lines.map(|x| x.to_string() + "\n").collect());
+    // let (start, target, graph) = parse_graph_p1(lines.map(|x| x.to_string() + "\n").collect());
+    let (start, target, graph) = parse_graph_p2(lines.map(|x| x.to_string() + "\n").collect());
+
+    println!("Navigating from {start:?} to {target:?}");
 
     let steps = steps_till(graph, directions, start, target);
     println!("{steps}");
 }
 
-fn steps_till (graph: Vec<NodeContents>, directions: Vec<bool>, start: Node, target: Node) -> u128 {
+fn steps_till(
+    graph: Vec<NodeContents>,
+    directions: Vec<bool>,
+    start: Vec<Node>,
+    target: Vec<Node>,
+) -> u128 {
+    if start.is_empty() {
+        return 0;
+    }
+
     let mut steps = 1;
 
     let mut current = start;
     let mut directions = directions.into_iter().cycle();
     loop {
-        
-        let node_contents = graph[current];
-        let new_node = if directions.next().unwrap() {
-            node_contents.left
-        } else {
-            node_contents.right
-        };
+        for current in current.iter_mut() {
+            let node_contents = graph[*current];
+            let new_node = if directions.next().unwrap() {
+                node_contents.left
+            } else {
+                node_contents.right
+            };
+            *current = new_node;
+        }
 
-        if new_node == target {
+        if current.iter().all(|x| target.contains(x)) {
             break;
         }
 
-        current = new_node;
         steps += 1;
     }
-
 
     steps
 }
 
 //first, target, nc
-fn parse_graph<'a> (lines: String) -> (Node, Node, Vec<NodeContents>) {
-    let variants: HashMap<String, usize> = lines.lines().into_iter().map(|l| (&l[0..3]).to_string()).enumerate().map(|(a, b)| (b, a)).collect();
+fn parse_graph_p1<'a>(lines: String) -> (Vec<Node>, Vec<Node>, Vec<NodeContents>) {
+    let variants: HashMap<String, usize> = lines
+        .lines()
+        .into_iter()
+        .map(|l| (&l[0..3]).to_string())
+        .enumerate()
+        .map(|(a, b)| (b, a))
+        .collect();
 
     let mut first = None;
     let mut map = vec![None; variants.len()];
@@ -53,18 +71,71 @@ fn parse_graph<'a> (lines: String) -> (Node, Node, Vec<NodeContents>) {
             first = Some(*start);
         }
 
-
         let left = variants.get(&line[7..10]).unwrap();
         let right = variants.get(&line[12..15]).unwrap();
 
         map[*start] = Some(NodeContents {
-            left: *left, right: *right
+            left: *left,
+            right: *right,
         });
     }
 
+    (
+        vec![variants.get("AAA").copied().unwrap()],
+        vec![variants.get("ZZZ").copied().unwrap()],
+        map.into_iter().map(|x| x.unwrap()).collect(),
+    )
+}
 
-    (variants.get("AAA").copied().unwrap(), variants.get("ZZZ").copied().unwrap(), map.into_iter().map(|x| x.unwrap()).collect())
+fn parse_graph_p2<'a>(lines: String) -> (Vec<Node>, Vec<Node>, Vec<NodeContents>) {
+    let variants: HashMap<String, usize> = lines
+        .lines()
+        .into_iter()
+        .map(|l| (&l[0..3]).to_string())
+        .enumerate()
+        .map(|(a, b)| (b, a))
+        .collect();
+    let starts = variants
+        .clone()
+        .into_iter()
+        .filter_map(|(s, idx)| {
+            if s.chars().last().unwrap() == 'A' {
+                Some(idx)
+            } else {
+                None
+            }
+        })
+        .collect();
+    let finishes = variants
+        .clone()
+        .into_iter()
+        .filter_map(|(s, idx)| {
+            if s.chars().last().unwrap() == 'Z' {
+                Some(idx)
+            } else {
+                None
+            }
+        })
+        .collect();
 
+    let mut map = vec![None; variants.len()];
+
+    for line in lines.lines() {
+        let start = variants.get(&line[0..3]).unwrap();
+        let left = variants.get(&line[7..10]).unwrap();
+        let right = variants.get(&line[12..15]).unwrap();
+
+        map[*start] = Some(NodeContents {
+            left: *left,
+            right: *right,
+        });
+    }
+
+    (
+        starts,
+        finishes,
+        map.into_iter().map(|x| x.unwrap()).collect(),
+    )
 }
 
 #[derive(Copy, Clone, Debug, Hash, Eq, PartialEq)]
